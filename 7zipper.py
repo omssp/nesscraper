8454ce97098fa39de780cd501174445ee2af7735
Shardul Pakhale <47375180+omssp@users.noreply.github.com>
2023-06-19 12:58:10 +0530

    pokemon girl hunter

--- 7zipper.py
+++ 7zipper.py
@@ -11,7 +11,7 @@ def sizeof_fmt(num, suffix="B"):
     return f"{num:.1f}Yi{suffix}"
 
 
-rom_path = './roms/*.nes'
+rom_path = './roms-gba/*.gba'
 
 roms = set(glob.glob(rom_path))
 roms_remove = set([x.replace('.7z', '') for x in glob.glob(f'{rom_path}.7z')])
@@ -20,8 +20,9 @@ roms = list(roms - roms_remove)
 
 for rom in roms:
     zFile = f'{rom}.7z'
+    rom_size = os.path.getsize(rom)
+    print(f"{sizeof_fmt(rom_size)} ", end='')
     with py7zr.SevenZipFile(zFile, 'w') as archive:
         archive.write(rom, os.path.basename(rom))
-    rom_size = os.path.getsize(rom)
     zFile_size = os.path.getsize(zFile)
-    print(f"{sizeof_fmt(rom_size)} --> {sizeof_fmt(zFile_size)} : {rom}")
+    print(f"--> {sizeof_fmt(zFile_size)} : {rom}")
