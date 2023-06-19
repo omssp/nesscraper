6ee92be8500412d9ef49d2c62e1a0de617cf6a65
Shardul Pakhale <47375180+omssp@users.noreply.github.com>
2023-06-19 12:40:58 +0530

    compressed roms

---
+++ 7zipper.py
@@ -0,0 +1,27 @@
+import py7zr
+import glob
+import os
+
+
+def sizeof_fmt(num, suffix="B"):
+    for unit in ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]:
+        if abs(num) < 1024.0:
+            return f"{num:3.1f}{unit}{suffix}"
+        num /= 1024.0
+    return f"{num:.1f}Yi{suffix}"
+
+
+rom_path = './roms/*.nes'
+
+roms = set(glob.glob(rom_path))
+roms_remove = set([x.replace('.7z', '') for x in glob.glob(f'{rom_path}.7z')])
+
+roms = list(roms - roms_remove)
+
+for rom in roms:
+    zFile = f'{rom}.7z'
+    with py7zr.SevenZipFile(zFile, 'w') as archive:
+        archive.write(rom, os.path.basename(rom))
+    rom_size = os.path.getsize(rom)
+    zFile_size = os.path.getsize(zFile)
+    print(f"{sizeof_fmt(rom_size)} --> {sizeof_fmt(zFile_size)} : {rom}")
